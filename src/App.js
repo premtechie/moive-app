66794b3850c869bbcadf0e91c789d5eab9fdf83f
Premkumar N <premkumar.n@highpeaksw.com>
2021-10-31 11:35:54 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const api = axios.create({ baseURL: BASE_URL });
+  const api_key = "ff21b822223b826772e86345f0833060";
+  const handleFetch = () =>{
+    axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`).then(response =>console.log(response))
+
+  }
+  handleFetch()
   return (
     <div className="App">
       <header className="App-header">
